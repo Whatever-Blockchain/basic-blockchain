269e39bf4947f9ba79d1738b3b41230cc8bc35a4
TaeGit <tgsong827@gmail.com>
2023-01-01 03:09:01 +0900

    feat: Transaction

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,6 @@
 use std::fmt::{self, Debug, Formatter};
 
-use crate::{hashable::Hashable, u32_bytes, u128_bytes, u64_bytes, difficulty_bytes_as_u128, Hash};
+use crate::{hashable::Hashable, u32_bytes, u128_bytes, u64_bytes, difficulty_bytes_as_u128, Hash, Transaction};
 
 pub struct Block {
     pub index: u32,
@@ -8,7 +8,7 @@ pub struct Block {
     pub hash: Hash,
     pub prev_block_hash: Hash,
     pub nonce: u64,
-    pub payload: String,
+    pub transactions: Vec<Transaction>,
     pub difficulty: u128,
 }
 
@@ -21,7 +21,7 @@ impl Debug for Block {
             &self.index,
             &hex::encode(&self.hash),
             &self.timestamp,
-            &self.payload,
+            &self.transactions.len(),
             &self.nonce,
         )
     }
@@ -32,8 +32,7 @@ impl Block {
         index: u32,
         timestamp: u128,
         prev_block_hash: Hash,
-        nonce: u64,
-        payload: String,
+        transactions: Vec<Transaction>,
         difficulty: u128
     ) -> Self {
         Block {
@@ -41,8 +40,8 @@ impl Block {
             timestamp,
             hash: vec![0; 32],
             prev_block_hash,
-            nonce,
-            payload,
+            nonce: 0,
+            transactions,
             difficulty,
         }
     }
@@ -67,7 +66,12 @@ impl Hashable for Block {
         bytes.extend(&u128_bytes(&self.timestamp));
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
-        bytes.extend(self.payload.as_bytes());
+        bytes.extend(
+            self.transactions
+                    .iter()
+                    .flat_map(|transaction| transaction.bytes())
+                    .collect::<Vec<u8>>()
+        );
         bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
