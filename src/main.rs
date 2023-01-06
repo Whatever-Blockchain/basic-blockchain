3c7e252097ea1c8d8e8be27445899bb8499c2720
TaeGit <tgsong827@gmail.com>
2023-01-06 22:17:09 +0900

    test fmt

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use basicblockchainlib::*;
 
 fn main() {
     let difficulty = 0x000fffffffffffff_ffffffffffffffff;
-    
-    let mut genesis_block = Block::new(0, now(), vec![0; 32], vec![
-        Transaction {
+
+    let mut genesis_block = Block::new(
+        0,
+        now(),
+        vec![0; 32],
+        vec![Transaction {
             inputs: vec![],
             outputs: vec![
                 transaction::Output {
@@ -14,10 +17,11 @@ fn main() {
                 transaction::Output {
                     to_addr: "Bob".to_owned(),
                     value: 7,
-                }
+                },
             ],
-        }
-    ], difficulty);
+        }],
+        difficulty,
+    );
 
     genesis_block.mine();
     println!("Mined genesis block {:?}", &genesis_block);
@@ -26,42 +30,45 @@ fn main() {
 
     let mut blockchain = Blockchain::new();
 
-    blockchain.update_with_bloc(genesis_block).expect("Failed to add genesis block");
+    blockchain
+        .update_with_bloc(genesis_block)
+        .expect("Failed to add genesis block");
 
-
-
-
-    let mut block = Block::new(1, now(), last_hash, vec![
-        Transaction {
-            inputs: vec![],
-            outputs: vec![
-                transaction::Output {
+    let mut block = Block::new(
+        1,
+        now(),
+        last_hash,
+        vec![
+            Transaction {
+                inputs: vec![],
+                outputs: vec![transaction::Output {
                     to_addr: "Chris".to_owned(),
                     value: 536,
-                },
-            ],
-        },
-        Transaction {
-            inputs: vec![
-                blockchain.blocks[0].transactions[0].outputs[0].clone(),
-            ],
-            outputs: vec![
-                transaction::Output {
-                    to_addr: "Alice".to_owned(),
-                    value: 360,
-                },
-                transaction::Output {
-                    to_addr: "Bob".to_owned(),
-                    value: 12,
-                },
-            ],
-        },
-    ], difficulty);
+                }],
+            },
+            Transaction {
+                inputs: vec![blockchain.blocks[0].transactions[0].outputs[0].clone()],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: "Alice".to_owned(),
+                        value: 360,
+                    },
+                    transaction::Output {
+                        to_addr: "Bob".to_owned(),
+                        value: 12,
+                    },
+                ],
+            },
+        ],
+        difficulty,
+    );
 
     block.mine();
     println!("Mined genesis block {:?}", &block);
 
     let mut last_hash = block.hash.clone();
 
-    blockchain.update_with_bloc(block).expect("Failed to add genesis block");
+    blockchain
+        .update_with_bloc(block)
+        .expect("Failed to add genesis block");
 }
